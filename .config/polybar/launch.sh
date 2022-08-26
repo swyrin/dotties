f318f38054a48a622e8339cd0773e600be84d054
Tien Dat Pham <dat20036@gmail.com>
2022-08-26 10:55:56 +0700

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 killall -q polybar
-polybar awesome 2>&1 | tee -a /tmp/polybar.log & disown
-echo "polybar launched..."
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar awesome &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload awesome &
+  done
+else
+  polybar --reload awesome &
+fi

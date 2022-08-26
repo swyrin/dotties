1dd9eae7f78fe3f82d250a96a5ae223916451bd4
Tien Dat Pham <dat20036@gmail.com>
2022-08-26 08:34:48 +0700

    More stuffs for bash installer

--- anything.sh
+++ anything.sh
@@ -3,12 +3,24 @@
 # ########### Arch with AwesomeWM install ###########
 # Yes, run this as sudo please, kthx!
 # No, this is not Ubuntu!
+# No, DO NOT BLINDLY RUN THIS FILE!!!!
 #
 # Basically this is just my autosetup file when I install a new thing!
 # Just clone this file, make anything.sh an executable
 
 # Setup parallelism for pacman
 echo "ParallelDownloads = 5" >> /etc/pacman.conf
+
+# I use archinstall with AwesomeWM profile, so X related packages are installed and properly set up
+# If you are using mininal install, or manually install from Arch Wiki, uncomment them!
+# REMEMBER TO REBOOT!!!! DO **NOT** USE `startx` AFTER THIS!!!
+# You *may* want to double check the $HOME/.xinitrc file!
+
+# sudo pacman -S --noconfirm xorg-server xorg-server-utils xorg-xinit mesa libva-intel-driver intel-media-driver vulkan-intel
+# sudo pacman -S --noconfirm alacritty
+# cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
+# sed -i 's/xterm/alacritty/g' $HOME/.xinitrc
+# echo "exec awesome" >> $HOME/.xinitrc
 
 # Setup my beloved nano
 sudo pacman -S --noconfirm nano
@@ -34,8 +46,8 @@ sudo pacman -S --noconfirm polybar
 # Install rofi
 sudo pacman -S --noconfirm rofi
 
-# Install picom fork
-sudo yay -S picom-ibahgwan-git --noconfirm
+# Install picom
+sudo pacman -S --noconfirm picom
 
 # Setup dotties
 cp .config $HOME/.config

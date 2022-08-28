3ee05108691e216711be44bfc655b6fb3e1795de
Tien Dat Pham <dat20036@gmail.com>
2022-08-28 11:00:45 +0700

    Delete .config/polybar/redshift directory

--- .config/polybar/redshift/env.sh
+++
@@ -1,7 +0,0 @@
-# JSON=$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | awk 'OFS=":" {print $3,$5}' | tr -d ',}')
-# RET=$(redshift -l $JSON -p 2> /dev/null)
-# TEMP=$(grep "Color temperature: " <<< $RET)
-# TEMP=$(sed "s/Color temperature: //g; s/K//g" <<< $TEMP)
-
-export REDSHIFT_TEMP=2700
-export REDSHIFT=on

c5f2905ef7a5795de7f57dc6fe5afbb6ade3f5fe
Tien Dat Pham <dat20036@gmail.com>
2022-08-28 15:38:58 +0700

    Update anything.sh

--- anything.sh
+++ anything.sh
@@ -93,6 +93,9 @@ sudo pacman -S --needed --noconfirm unrar
 sudo rm -rf $HOME/.config/
 sudo chmod 777 -R $DOTTIES_DIR/.config/
 sudo ln -s $DOTTIES_DIR/.config/ $HOME/
+sudo ln -sf $DOTTIES_DIR/.p10k.zsh ~/.p10k.zsh
+sudo ln -sf $DOTTIES_DIR/.zshrc $HOME/.zshrc
+sudo ln -sf $DOTTIES_DIR/.fehbg ~/.fehbg
 
 # Export some environment variables
 export TERM=kitty

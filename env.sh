a7048aa227beda817f7313ac87ccec573919cb0d
Tien Dat Pham <dat20036@gmail.com>
2022-09-18 10:14:52 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,8 @@
 DOTTIES_DIR=$(find $HOME -type d -name "dotties")
+VIS_ALIAS="alias vis='$DOTTIES_DIR/viswal.sh'"
+POP_REPORT_ALIAS="alias pop_report='$DOTTIES_DIR/pop_report'"
 
-echo "alias vis='$DOTTIES_DIR/viswal.sh'" | tee -a $HOME/.bashrc
-echo "alias pop_report='$DOTTIES_DIR/pop_report'" | tee -a $HOME/.bashrc
+echo $VIS_ALIAS | tee -a $HOME/.bashrc
+echo $VIS_ALIAS | tee -a $HOME/.zshrc
+echo $POP_REPORT_ALIAS | tee -a $HOME/.bashrc
+echo $POP_REPORT_ALIAS | tee -a $HOME/.zshrc

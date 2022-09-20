723d1f662b0d8910f0d45df1564c24eb3317a5c0
Tien Dat Pham <dat20036@gmail.com>
2022-09-20 10:18:19 +0700

    tee to multiple files

--- env.sh
+++ env.sh
@@ -2,7 +2,5 @@ DOTTIES_DIR=$(find $HOME -type d -name "dotties")
 VIS_ALIAS="alias vis='$DOTTIES_DIR/viswal.sh'"
 POP_REPORT_ALIAS="alias pop_report='$DOTTIES_DIR/pop_report'"
 
-echo $VIS_ALIAS | tee -a $HOME/.bashrc
-echo $VIS_ALIAS | tee -a $HOME/.zshrc
-echo $POP_REPORT_ALIAS | tee -a $HOME/.bashrc
-echo $POP_REPORT_ALIAS | tee -a $HOME/.zshrc
+echo $VIS_ALIAS | tee -a $HOME/.bashrc $HOME/.zshrc
+echo $POP_REPORT_ALIAS | tee -a $HOME/.bashrc $HOME/.zshrc

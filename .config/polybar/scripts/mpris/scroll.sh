6409d348e674e84366757c540b6fb02cc0101ce8
Foxeiz <foxeiz2004@gmail.com>
2022-09-23 21:37:38 +0700

    increase delay to minimize spam in dbus

--- .config/polybar/scripts/mpris/scroll.sh
+++ .config/polybar/scripts/mpris/scroll.sh
@@ -2,7 +2,7 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
-  --delay 0.1 \
+  --delay 5 \
   --match-command "$(dirname $0)/mpris_control.sh --title" \
   --update-check true "$(dirname $0)/mpris_control.sh --title" &
 

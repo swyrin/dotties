d29dc5d28159f824f73198f6635b56e5995f0467
Tien Dat Pham <dat20036@gmail.com>
2023-01-19 22:40:29 +0700

    debloat and tidy up the files

--- anything.sh
+++ anything.sh
@@ -10,54 +10,39 @@
 # ONE MORE LAST WORD: THIS SCRIPT IS MEANT FOR **ME**!!!!!!!!!!!!!!!!!
 
 # For debugging sake, but it *should* work as I want
-echo "Finding 'dotties' directory... This should not take long"
 DOTTIES_DIR=$(find $HOME -type d -name "dotties")
-echo "Found and using dotties directory $DOTTIES_DIR"
 echo "The script *might* ask you the password below"
 
 # Setup custom pacman stuffs: parallel downloads, c o l o r s
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
 
+# fast commands
+PACMAN="sudo pacman -S --needed --noconfirm"
+YAY="yay -S --needed --noconfirm --removemake"
+SYSCTL_ENABLE="sudo systemctl enable --now"
+SYSCTL_ENABLE_USER="systemctl enable --user --now"
+
 # As usual, pacman -Syu
 sudo pacman -Syu --noconfirm
 
-# Install server packages
-# I assume that you will run this file after booting Arch for the first time
-sudo pacman -S --needed --noconfirm pulseaudio networkmanager
-sudo systemctl enable NetworkManager.service
+# Install packages for sounds and internet
+$PACMAN networkmanager \
+        pulseaudio
+$SYSCTL_ENABLE NetworkManager.service
 
 # Install Xorg and friends
-sudo pacman -S --needed --noconfirm xorg xorg-apps xorg-server xorg-xinit \
-                                    mesa mesa-utils libva-intel-driver mesa-amber \
-                                    intel-media-driver vulkan-intel \
-                                    xf86-video-nouveau xf86-video-intel
+$PACMAN xorg xorg-apps xorg-xinit \
+        mesa mesa-utils libva-intel-driver mesa-amber \
+        intel-media-driver vulkan-intel \
+        xf86-video-nouveau xf86-video-intel
 
 # Setup my beloved nano
-sudo pacman -S --needed --noconfirm nano
-sudo pacman -S --needed --noconfirm unzip wget
+$PACMAN nano unzip wget
 wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
-
-if [ -z $(grep "set linenumbers" $HOME/.nanorc) ]
-then
-  echo "set linenumbers" >> $HOME/.nanorc
-else
-  echo "'set linenumbers' set in $HOME/.nanorc"
-fi
-
-if [ -z $(grep "set tabstospaces" $HOME/.nanorc) ]
-then
-  echo "set tabstospaces" >> $HOME/.nanorc
-else
-  echo "'set tabstospaces' set in $HOME/.nanorc"
-fi
-
-if [ -z $(grep "set tabsize 4" $HOME/.nanorc) ]
-then
-  echo "set tabsize 4" >> $HOME/.nanorc
-else
-  echo "'set tabsize 4' set in $HOME/.nanorc"
-fi
+grep "set linenumbers" $HOME/.nanorc || echo "set linenumbers" >> $HOME/.nanorc
+grep "set tabstospaces" $HOME/.nanorc || echo "set tabstospaces" >> $HOME/.nanorc
+grep "set tabsize 4" $HOME/.nanorc || echo "set tabsize 4" >> $HOME/.nanorc
 
 # Install yay AUR helper
 if [ -z $(which yay) ]
@@ -67,70 +52,50 @@ then
   makepkg -si --noconfirm
   cd ..
   sudo rm -r yay
-else
-  echo "AUR helper yay already installed"
 fi
 
 # Install BSPWM, rofi, picom (fork), sxhkd, polybar, notification center(TM), eww
 # Technically, setup the desktop
-sudo pacman -S --needed --noconfirm rofi sxhkd feh
-yay -S --needed --noconfirm --removemake eww
-yay -S --needed --noconfirm --removemake bspwm-git
-yay -S --needed --noconfirm --removemake picom-animations-git
-yay -S --needed --noconfirm --removemake polybar-git
-yay -S --needed --noconfirm --removemake deadd-notification-center-bin notify-send-py
+$PACMAN rofi sxhkd feh
+$YAY    eww \
+        bspwm-git \
+        picom-animations-git \
+        polybar-git \
+        deadd-notification-center-bin notify-send-py
 
-# Install and enable LightDM
-sudo pacman -S --needed --noconfirm lightdm lightdm-gtk-greeter
-sudo systemctl enable lightdm.service
-
-if [ -z $(which vis) ]
-then
-  # Install cli-visualizer and override with PyWal colors
-  sudo pacman -S --needed --noconfirm ncurses fftw cmake
-  git clone https://github.com/dpayne/cli-visualizer.git
-  cd cli-visualizer
-  ./install.sh
-  cd ..
-  sudo rm -rf cli-visualizer
-else
-  echo "cli-visualizer already installed"
-fi
-
-# Install stuffs for bars
-sudo pacman -S --needed --noconfirm acpi alsa-utils playerctl sysstat xdotool jq bc brightnessctl
+# Install stuffs for notifications
+$PACMAN playerctl brightnessctl
 
 # Install stuffs for system tray
-sudo pacman -S --needed --noconfirm redshift python-gobject
-sudo pacman -S --needed --noconfirm network-manager-applet
-sudo pacman -S --needed --noconfirm pavucontrol pasystray
-sudo pacman -S --needed --noconfirm flameshot
+$PACMAN redshift python-gobject \
+        network-manager-applet \
+        pavucontrol pasystray \
+        flameshot
 
-# Install fonts
+# Setup fonts
 # References: https://www.reddit.com/r/archlinux/comments/a2g77x/what_are_your_default_font_packages_you_install/
 sudo mkdir -p /usr/share/fonts/ && sudo cp -a $DOTTIES_DIR/fonts/. /usr/share/fonts/
 
-sudo pacman -S  --needed --noconfirm  ttf-dejavu ttf-liberation ttf-font-awesome ttf-liberation ttf-droid ttf-ubuntu-font-family \
-                                      noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
-                                      adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
+$PACMAN ttf-dejavu ttf-liberation ttf-font-awesome ttf-liberation ttf-droid ttf-ubuntu-font-family \
+        noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
+        adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
 
-yay -S  --needed --noconfirm --removemake noto-fonts-tc \
-                                          siji-git \
-                                          nerd-fonts-complete \
-                                          ttf-unifont ttf-gelasio-ib ttf-caladea ttf-material-design-icons ttf-carlito ttf-liberation-sans-narrow ttf-ms-fonts ttf-material-icons-git
+$YAY noto-fonts-tc \
+     siji-git \
+     nerd-fonts-complete \
+     tf-unifont ttf-gelasio-ib ttf-caladea ttf-material-design-icons ttf-carlito ttf-liberation-sans-narrow ttf-ms-fonts ttf-material-icons-git
 
-# Install Greenclip
-yay -S --needed --noconfirm --removemake rofi-greenclip
 
-# Install GTK themes
+# Setup GTK themes
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 wget -qO- https://git.io/papirus-folders-install | sh
 yay -S --needed --noconfirm --removemake catppuccin-gtk-theme-latte
 papirus-folders -C yellow --theme Papirus-Light
 
 # Setup needed packages (for me, you should change) as final run!!!
+# - Greenclip           - Clipboard manager for rofi
 # - Thunar              - File manager (+plugins,mpv,xfce-polkit)
-# - neofetch            - Neofetch
+# - pfetch              - Neofetch but simplified (+cat patch)
 # - btop                - Better htop
 # - Viewnior            - Image viewer
 # - Geany               - GUI text editor
@@ -139,14 +104,27 @@ papirus-folders -C yellow --theme Papirus-Light
 # - font-manager        - Font manager
 # - Peazip              - Archive manager
 # - XFCE Power Manager  - Brightness and stuffs
-# - Komorebi            - Live wallpaper setter (+extensions)
-sudo pacman -S --needed --noconfirm thunar neofetch btop viewnior geany kitty xclip font-manager
-sudo pacman -S --needed --noconfirm gvfs tumbler ffmpegthumbnailer poppler-glib libgsf libgepub libopenraw freetype2 thunar-volman thunar-archive-plugin thunar-media-tags-plugin mpv
-yay -S --needed --noconfirm --removemake peazip-gtk2-bin komorebi
-sudo pacman -S --needed --noconfirm gst-libav gstreamer-vaapi gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly libde265
-yay -S --needed --noconfirm --removemake gst-plugin-libde265
-yay -S --needed --noconfirm --removemake xfce-polkit
-sudo pacman -S --needed --noconfirm xfce4-power-manager
+# - Komorebi            - Live wallpaper setter (+extensions) -
+$YAY    rofi-greenclip \
+        pfetch \
+        peazip-gtk2-bin \
+        komorebi \
+        gst-plugin-libde265 \
+        xfce-polkit
+
+$PACMAN thunar gvfs tumbler ffmpegthumbnailer poppler-glib libgsf libgepub libopenraw freetype2 thunar-volman thunar-archive-plugin thunar-media-tags-plugin mpv \
+        btop \
+        viewnior \
+        geany \
+        kitty \
+        xclip \
+        font-manager \
+        xfce4-power-manager
+
+# Stuffs for komorebi
+$PACMAN gst-libav gstreamer-vaapi \
+        gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly \
+        libde265
 
 # Setup files
 mv $HOME/.config/ $HOME/.config_backup/
@@ -166,61 +144,63 @@ sudo ln -sf $DOTTIES_DIR/.config/eww/ $HOME/.config/
 sudo ln -sf $DOTTIES_DIR/.config/komorebi/ $HOME/.config/
 sudo ln -sf $DOTTIES_DIR/.config/deadd/ $HOME/.config/
 sudo ln -sf $DOTTIES_DIR/.zshrc $HOME/.zshrc
+sudo ln -sf $DOTTIES_DIR/.xinitrc $HOME/.xinitrc
 sudo ln -sf $DOTTIES_DIR/.p10k.zsh $HOME/.p10k.zsh
 sudo ln -sf $DOTTIES_DIR/.gtkrc-2.0 $HOME/.gtkrc-2.0
+cp -a $DOTTIES_DIR/.config_backup/. $DOTTIES_DIR/.config/
+rm -rf $DOTTIES_DIR/.config_backup/
 
 # Setup battery saving "bloatwares"
 if ! [ -z $(upower) ];
 then
-    # You should consume these links for more battery saving
-    # https://wiki.archlinux.org/title/Power_management
-    # https://wiki.archlinux.org/title/CPU_frequency_scaling
-
     # Intel HW acceleration
-    sudo pacman -S --needed --noconfirm intel-media-driver libva-intel-driver
+    $PACMAN intel-media-driver libva-intel-driver
     sudo sed -i 's/MODULES=()/MODULES=(i915,i965,iHD)/g' /etc/mkinitcpio.conf
     sudo mkinitcpio -P
 
     # Powertop
-    sudo pacman -S --needed --noconfirm powertop
+    $PACMAN powertop
     sudo cp $DOTTIES_DIR/.config/systemd/system/powertop.service /etc/systemd/system/powertop.service
-    sudo systemctl enable --now powertop.service
+    $SYSCTL_ENABLE powertop.service
 
-    # Laptop setup
-    sudo pacman -S --needed --noconfirm acpid
-    sudo systemctl enable --now acpid.service
+    # Laptop setup (acpid)
+    $PACMAN acpid
+    $SYSCTL_ENABLE acpid.service
 
-    sudo pacman -S --needed --noconfirm tlp tlp-rdw
-    yay -S --needed --noconfirm --removemake tlpui
-    sudo systemctl enable --now tlp.service
+    # TLP
+    $PACMAN tlp tlp-rdw
+    $YAY tlpui
+    $SYSCTL_ENABLE tlp.service
     sudo systemctl mask systemd-rfkill.service
     sudo systemctl mask systemd-rfkill.socket
 
-    sudo pacman -S --needed --noconfirm thermald
-    sudo systemctl enable --now thermald.service
+    # thermald
+    $PACMAN thermald
+    $SYSCTL_ENABLE thermald.service
 
-    yay -S --needed --noconfirm --removemake auto-cpufreq
-    sudo systemctl enable --now auto-cpufreq.service
+    # auto-cpufreq
+    $YAY auto-cpufreq
+    $SYSCTL_ENABLE auto-cpufreq.service
 else
     echo "No battery installed, skipping installing power savers"
 fi
 
 # Post-install stuffs
-systemctl enable --user sxhkd.service
-systemctl enable --user greenclip.service
-sudo touch $HOME/.config/polybar/.curplayer.log
+$SYSCTL_ENABLE_USER sxhkd.service
+$SYSCTL_ENABLE_USER greenclip.service
 
 # Clear PM cache
 sudo pacman -Sc
 yay -Sc
 
 # What to do after this:
-#     1. If you are using a touchpad: https://stackoverflow.com/questions/62990795/cannot-set-tapping-enabled-default-on-archlinux
-#     2. If your keyboard has a NumLk: https://wiki.archlinux.org/title/Activating_numlock_on_bootup
+#       1. If you are using a touchpad: https://stackoverflow.com/questions/62990795/cannot-set-tapping-enabled-default-on-archlinux
+#       2. If your keyboard has a NumLk: https://wiki.archlinux.org/title/Activating_numlock_on_bootup
+#       3. You should consume these links for more battery saving on laptops:
+#           https://wiki.archlinux.org/title/Power_management
+#           https://wiki.archlinux.org/title/CPU_frequency_scaling
 
 # Bye!
 echo "Installation finished!!!"
 echo "And please, for the love of god, DO **NOT** REMOVE THE $DOTTIES_DIR FOLDER!!!! Why? They are **ALL SYMLINKED**!"
-echo "Remember to check $HOME/.config_backup directory"
-echo "AND REMEMBER TO REBOOT!!!! DO **NOT** USE 'startx' AFTER THIS!!!"
 echo "That is all, have a good day and happy ricing!"

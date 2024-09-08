41c4de2f41011aea68a00ff88513d3a7bc93f3ff
Tien Dat Pham <dat20036@gmail.com>
2024-09-08 22:50:35 +0700

    clean up anything.sh finished

--- anything.sh
+++ anything.sh
@@ -36,6 +36,21 @@ THIS_PLACE=$(pwd)
 . $THIS_PLACE/install_scripts/install_fonts.sh
 . $THIS_PLACE/install_scripts/setup_system.sh
 
+# Setup symbolic links
+ln -sf $THIS_PLACE/.config/alacritty           $HOME/.config/alacritty
+ln -sf $THIS_PLACE/.config/waybar              $HOME/.config/waybar
+ln -sf $THIS_PLACE/.config/rofi 				$HOME/.config/rofi
+ln -sf $DOTTIES_DIT/.config/hypr                $HOME/.config/hypr
+ln -sf $THIS_PLACE/.config/gtk-3.0 			$HOME/.config/gtk-3.0
+ln -sf $THIS_PLACE/.config/gtk-4.0 			$HOME/.config/gtk-4.0
+ln -sf $THIS_PLACE/.config/chrome-flags.conf 	$HOME/.config/chrome-flags.conf
+ln -sf $THIS_PLACE/.config/chrome-flags.conf 	$HOME/.config/electron-flags.conf
+ln -sf $THIS_PLACE/.zshrc 						$HOME/.zshrc
+ln -sf $THIS_PLACE/.zprofile                   $HOME/.zprofile
+ln -sf $THIS_PLACE/.p10k.zsh 					$HOME/.p10k.zsh
+ln -sf $THIS_PLACE/.nanorc                     $HOME/.nanorc
+
+
 # -- HIGHLY NOT RECOMMENDED.
 # -- Un-comment if you are using a laptop
 # -- AND OBSESSED WITH 1.0 W USAGE.
@@ -43,35 +58,3 @@ THIS_PLACE=$(pwd)
 
 # Post-installation
 . $THIS_PLACE/install_scripts/post_install.sh
-
-# Symlinking files
-ln -sf $DOTTIES_DIR/.config/autostart 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/bspwm 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/polybar 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/systemd 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/kitty 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/picom 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/Thunar 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/rofi 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/flameshot 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/gtk-3.0 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/gtk-4.0 			$HOME/.config
-ln -sf $DOTTIES_DIR/.config/eww 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/dunst 				$HOME/.config
-ln -sf $DOTTIES_DIR/.config/chrome-flags.conf 	$HOME/.config/chrome-flags.conf
-ln -sf $DOTTIES_DIR/.zshrc 						$HOME/.zshrc
-ln -sf $DOTTIES_DIR/.xinitrc 					$HOME/.xinitrc
-ln -sf $DOTTIES_DIR/.p10k.zsh 					$HOME/.p10k.zsh
-ln -sf $DOTTIES_DIR/.gtkrc-2.0 					$HOME/.gtkrc-2.0
-
-# What to do after this:
-#       1. If you are using a touchpad: https://stackoverflow.com/questions/62990795/cannot-set-tapping-enabled-default-on-archlinux
-#       2. If your keyboard has a NumLk: https://wiki.archlinux.org/title/Activating_numlock_on_bootup
-#       3. You should consume these links for more battery saving on laptops:
-#           https://wiki.archlinux.org/title/Power_management
-#           https://wiki.archlinux.org/title/CPU_frequency_scaling
-#           https://www.reddit.com/r/archlinux/comments/rz6294/arch_linux_laptop_optimization_guide_for/
-#       4. If you can, use `linux-zen` kernel instead.
-echo "Installation finished!!!"
-echo "And please, for the love of god, DO **NOT** REMOVE THE $DOTTIES_DIR FOLDER!!!! Why? They are **ALL SYMLINKED**!"
-echo "That is all, have a good day and happy ricing!"

22f71334544a30cf461237046868676d75977967
Tien Dat Pham <dat20036@gmail.com>
2024-09-09 07:49:07 +0700

    Remove ttf-symbola from font list.

--- install_scripts/install_fonts.sh
+++ install_scripts/install_fonts.sh
@@ -7,12 +7,14 @@ PARU="paru -S --needed --noconfirm --removemake"
 
 # https://www.reddit.com/r/archlinux/comments/a2g77x/what_are_your_default_font_packages_you_install/
 
-$PACMAN ttf-nerd-fonts-symbols-1000-em-mono ttf-nerd-fonts-symbols-common \
+$PACMAN ttf-hack ttf-fira-sans \
+        ttf-nerd-fonts-symbols-mono ttf-nerd-fonts-symbols-common \
         ttf-dejavu ttf-liberation ttf-font-awesome ttf-liberation ttf-droid ttf-ubuntu-font-family \
         ttf-jetbrains-mono-nerd \
+        otf-font-awesome \
         noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji \
         adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
 
 $PARU noto-fonts-tc \
      siji-git \
-     ttf-unifont ttf-gelasio-ib ttf-caladea ttf-carlito ttf-liberation-sans-narrow ttf-ms-fonts ttf-symbola
+     ttf-unifont ttf-gelasio-ib ttf-caladea ttf-carlito ttf-liberation-sans-narrow ttf-ms-fonts

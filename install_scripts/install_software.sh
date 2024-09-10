b59bc313935709bc9353bf730b0f662b8bc9ecef
Tien Dat Pham <dat20036@gmail.com>
2024-09-10 08:00:10 +0700

    New terminal font.

--- install_scripts/install_software.sh
+++ install_scripts/install_software.sh
@@ -16,12 +16,13 @@ $PACMAN pavucontrol			            # --- GUI tool for controlling audio
 $PACMAN gammastep			            # --- shut up blue light
 $PACMAN btop 				            # --- think like task manager
 $PACMAN zsh				                # --- z shell
-$PACMAN zsh-theme-powerlevel10k	        # --- z shell but on steroid
+$PARU   zsh-theme-powerlevel10k-bin-git     # --- z shell but on steroid
+$PARU   ttf-meslo-nerd-font-powerlevel10k   # --- PL10k recommended font
 $PACMAN zsh-completions			        # --- z shell completions
 $PACMAN zsh-autosuggestions             # --- z shell suggestions
 $PACMAN zsh-syntax-highlighting         # --- z shell syntax highlighting
 $PACMAN zsh-history-substring-search    # --- z shell command hint based on history
-$PACMAN fastfetch                        # --- system fetch
+$PACMAN fastfetch                       # --- system fetch
 $PACMAN alsa-utils                      # --- CLI tool for controlling audio, but works closely with Linux
 $PACMAN brightnessctl                   # --- CLI tool for controlling brightness
 $PACMAN powerline-fonts                 # --- powerline fonts

34b04c93c2b2e4e28a3c26ef028261ecac834b5a
Tien Dat Pham <dat20036@gmail.com>
2024-09-11 13:57:24 +0700

    add cliphist

--- install_scripts/install_software.sh
+++ install_scripts/install_software.sh
@@ -4,29 +4,29 @@ set -euox pipefail
 PACMAN="sudo pacman -S --needed --noconfirm"
 PARU="paru -S --needed --noconfirm --removemake"
 
-$PACMAN mako				            # --- libappnotify-compliant notification daemon
-$PACMAN waybar				            # --- the bar 
-$PACMAN alacritty        		        # --- terminal emulator
-$PACMAN rofi-wayland     		        # --- application launcher
-$PACMAN fcitx5{,-unikey}		        # --- input method (plus I am from 'nam)
-$PACMAN network-manager-applet	        # --- tray icon for networkmanager
-$PACMAN playerctl			            # --- CLI tool for controlling MPRIS-compliant media
-$PACMAN pamixer				            # --- CLI tool for controlling audio
-$PACMAN pavucontrol			            # --- GUI tool for controlling audio
-$PACMAN gammastep			            # --- shut up blue light
-$PACMAN btop 				            # --- think like task manager
-$PACMAN zsh				                # --- z shell
+$PACMAN mako				                # --- libappnotify-compliant notification daemon
+$PACMAN waybar				                # --- the bar 
+$PACMAN alacritty        		            # --- terminal emulator
+$PACMAN rofi-wayland     		            # --- application launcher
+$PARU   cliphist                            # --- clipboard history
+$PACMAN fcitx5{,-unikey}		            # --- input method (plus I am from 'nam)
+$PACMAN network-manager-applet	            # --- tray icon for networkmanager
+$PACMAN playerctl			                # --- CLI tool for controlling MPRIS-compliant media
+$PACMAN pamixer				                # --- CLI tool for controlling audio
+$PACMAN pavucontrol			                # --- GUI tool for controlling audio
+$PACMAN gammastep			                # --- shut up blue light
+$PACMAN btop 				                # --- think like task manager
+$PACMAN zsh				                    # --- z shell
 $PARU   zsh-theme-powerlevel10k-bin-git     # --- z shell but on steroid
 $PARU   ttf-meslo-nerd-font-powerlevel10k   # --- PL10k recommended font
-$PACMAN zsh-completions			        # --- z shell completions
-$PACMAN zsh-autosuggestions             # --- z shell suggestions
-$PACMAN zsh-syntax-highlighting         # --- z shell syntax highlighting
-$PACMAN zsh-history-substring-search    # --- z shell command hint based on history
-$PACMAN fastfetch                       # --- system fetch
-$PACMAN alsa-utils                      # --- CLI tool for controlling audio, but works closely with Linux
-$PACMAN brightnessctl                   # --- CLI tool for controlling brightness
-$PACMAN powerline-fonts                 # --- powerline fonts
-$PACMAN fzf                             # --- (f)u(z)zy (f)inder
-$PACMAN pkgfile                         # --- Arch repo package lookup 
-
-$PARU oh-my-zsh-git                     # --- oh my zsh for powerlevel10k
+$PACMAN zsh-completions			            # --- z shell completions
+$PACMAN zsh-autosuggestions                 # --- z shell suggestions
+$PACMAN zsh-syntax-highlighting             # --- z shell syntax highlighting
+$PACMAN zsh-history-substring-search        # --- z shell command hint based on history
+$PACMAN fastfetch                           # --- system fetch
+$PACMAN alsa-utils                          # --- CLI tool for controlling audio, but works closely with Linux
+$PACMAN brightnessctl                       # --- CLI tool for controlling brightness
+$PACMAN powerline-fonts                     # --- powerline fonts
+$PACMAN fzf                                 # --- (f)u(z)zy (f)inder
+$PACMAN pkgfile                             # --- Arch repo package lookup 
+$PARU   oh-my-zsh-git                       # --- oh my zsh for powerlevel10k

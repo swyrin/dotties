765be514c5f5198d89a47d24f931d3e71b8caac2
Tien Dat Pham <dat20036@gmail.com>
2024-09-12 19:47:14 +0700

    Enable "literal PacMan" in `pacman`.

--- install_scripts/configure_pacman.sh
+++ install_scripts/configure_pacman.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo sed -i 's/#ILoveCandy/ILoveCandy/g' /etc/pacman.conf
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf

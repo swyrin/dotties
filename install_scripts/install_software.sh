28672bbf38b0776bab31a53884931bf62c7639b3
Tien Dat Pham <dat20036@gmail.com>
2024-10-08 09:02:13 +0700

    tab tab tab tab

--- install_scripts/install_software.sh
+++ install_scripts/install_software.sh
@@ -4,28 +4,35 @@ set -euox pipefail
 PACMAN="sudo pacman -S --needed --noconfirm"
 PARU="paru -S --needed --noconfirm --removemake"
 
-$PACMAN alacritty        		                # --- terminal emulator
-$PACMAN rofi-wayland     		                # --- application launcher
-$PARU   rofimoji                                # --- emoji picker for rofi
-$PARU   cliphist wtype                          # --- clipboard history for nwg-clipman
-$PACMAN nwg-clipman                             # --- clipboard manager
-$PACMAN playerctl			                    # --- CLI tool for controlling MPRIS-compliant media
-$PACMAN pamixer				                    # --- CLI tool for controlling audio
-$PACMAN pavucontrol			                    # --- GUI tool for controlling audio
-$PACMAN zsh				                        # --- z shell
-$PARU   zsh-theme-powerlevel10k-git             # --- z shell but on steroid
-$PARU   ttf-meslo-nerd-font-powerlevel10k       # --- PL10k recommended font
-$PACMAN zsh-completions			                # --- z shell completions on tab
-$PACMAN zsh-autosuggestions                     # --- z shell suggestions (as a shadow text)
-$PACMAN zsh-syntax-highlighting                 # --- z shell syntax highlighting
-$PACMAN zsh-history-substring-search            # --- z shell command hint based on history
-$PACMAN fastfetch                               # --- system fetch
-$PACMAN alsa-utils                              # --- CLI tool for controlling audio, but works closely with Linux
-$PACMAN brightnessctl                           # --- CLI tool for controlling brightness
-$PACMAN powerline-fonts                         # --- powerline fonts
-$PACMAN fzf                                     # --- (f)u(z)zy (f)inder
-$PACMAN pkgfile                                 # --- Arch repo package lookup
-$PARU   oh-my-zsh-git                           # --- oh my zsh for powerlevel10k
-$PARU   spoofdpi-bin                            # --- DPI spoofer
+$PACMAN gnome-keyring libsecret libgnome-keyring	# --- GNOME keyring
+$PACMAN nautilus					                # --- GNOME Files
+$PACMAN file-roller					                # --- GNOME archive manager
+$PACMAN seahorse					                # --- GNOME secret manager
+$PACMAN firefox						                # --- Browser
+$PACMAN btop						                # --- Task manager but on steroid
+$PACMAN network-manager-applet				        # --- NM tray icon
+$PACMAN alacritty        		                    # --- terminal emulator
+$PACMAN rofi-wayland     		                    # --- application launcher
+$PARU   rofimoji                                	# --- emoji picker for rofi
+$PARU   cliphist wtype                          	# --- clipboard history for nwg-clipman
+$PACMAN nwg-clipman                             	# --- clipboard manager
+$PACMAN playerctl			                        # --- CLI tool for controlling MPRIS-compliant media
+$PACMAN pamixer				                        # --- CLI tool for controlling audio
+$PACMAN pavucontrol			                        # --- GUI tool for controlling audio
+$PACMAN zsh				                            # --- z shell
+$PARU   zsh-theme-powerlevel10k-git             	# --- z shell but on steroid
+$PARU   ttf-meslo-nerd-font-powerlevel10k       	# --- PL10k recommended font
+$PACMAN zsh-completions			                    # --- z shell completions on tab
+$PACMAN zsh-autosuggestions                     	# --- z shell suggestions (as a shadow text)
+$PACMAN zsh-syntax-highlighting                 	# --- z shell syntax highlighting
+$PACMAN zsh-history-substring-search            	# --- z shell command hint based on history
+$PACMAN fastfetch                               	# --- system fetch
+$PACMAN alsa-utils                              	# --- CLI tool for controlling audio, but works closely with Linux
+$PACMAN brightnessctl                           	# --- CLI tool for controlling brightness
+$PACMAN powerline-fonts                         	# --- powerline fonts
+$PACMAN fzf                                     	# --- (f)u(z)zy (f)inder
+$PACMAN pkgfile                                 	# --- Arch repo package lookup
+$PARU   oh-my-zsh-git                           	# --- oh my zsh for powerlevel10k
+$PARU   spoofdpi-bin                            	# --- DPI spoofer
 $PACMAN nano						                # --- simple editor runs on terminal
 $PARU 	nano-syntax-highlighting-git			    # --- nano syntax highlighting
--- install_scripts/symlink.sh
+++ install_scripts/symlink.sh
@@ -8,21 +8,21 @@ THIS_PLACE="$(dirname "$THIS_PLACE")"
 
 LINK="ln -snf"
 
-$LINK $THIS_PLACE/.config/alacritty             $HOME/.config/alacritty
-$LINK $THIS_PLACE/.config/waybar                $HOME/.config/waybar
-$LINK $THIS_PLACE/.config/rofi 				    $HOME/.config/rofi
-$LINK $THIS_PLACE/.config/hypr                  $HOME/.config/hypr
-$LINK $THIS_PLACE/.config/gtk-3.0 			    $HOME/.config/gtk-3.0
-$LINK $THIS_PLACE/.config/mako                  $HOME/.config/mako
-$LINK $THIS_PLACE/.config/fastfetch             $HOME/.config/fastfetch
-$LINK $THIS_PLACE/.config/wlogout               $HOME/.config/wlogout
-$LINK $THIS_PLACE/.config/qt5ct                 $HOME/.config/qt5ct
-$LINK $THIS_PLACE/.config/qt6ct                 $HOME/.config/qt6ct
-$LINK $THIS_PLACE/.config/eww                   $HOME/.config/eww
-$LINK $THIS_PLACE/.zshrc 						$HOME/.zshrc
-$LINK $THIS_PLACE/.p10k.zsh 			$HOME/.p10k.zsh
+$LINK $THIS_PLACE/.config/alacritty                         $HOME/.config/alacritty
+$LINK $THIS_PLACE/.config/waybar                            $HOME/.config/waybar
+$LINK $THIS_PLACE/.config/rofi 			                    $HOME/.config/rofi
+$LINK $THIS_PLACE/.config/hypr                              $HOME/.config/hypr
+$LINK $THIS_PLACE/.config/gtk-3.0 		                    $HOME/.config/gtk-3.0
+$LINK $THIS_PLACE/.config/mako                              $HOME/.config/mako
+$LINK $THIS_PLACE/.config/fastfetch                         $HOME/.config/fastfetch
+$LINK $THIS_PLACE/.config/wlogout                           $HOME/.config/wlogout
+$LINK $THIS_PLACE/.config/qt5ct                             $HOME/.config/qt5ct
+$LINK $THIS_PLACE/.config/qt6ct                             $HOME/.config/qt6ct
+$LINK $THIS_PLACE/.config/eww                               $HOME/.config/eww
+$LINK $THIS_PLACE/.zshrc 			                        $HOME/.zshrc
+$LINK $THIS_PLACE/.p10k.zsh 			                    $HOME/.p10k.zsh
 $LINK $THIS_PLACE/.nanorc			                        $HOME/.nanorc
 
-$LINK $THIS_PLACE/assets/wallpaper.png          $HOME/Pictures/wallpaper.png
+$LINK $THIS_PLACE/assets/wallpaper.png                      $HOME/Pictures/wallpaper.png
 
 sudo $LINK /usr/share/fontconfig/conf.avail/75-twemoji.conf /etc/fonts/conf.d/75-twemoji.conf

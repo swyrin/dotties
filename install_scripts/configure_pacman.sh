32f3e683ac1b579af018ba925d8c98a14addc921
Tien Dat Pham <dat20036@gmail.com>
2024-10-08 18:26:52 +0700

    tweak pacman configuration script

--- install_scripts/configure_pacman.sh
+++ install_scripts/configure_pacman.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo sed -i 's/#ILoveCandy/ILoveCandy/g' /etc/pacman.conf
-sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
-sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+# --- Enable output color, with a hack to enable pacman effect
+sudo sed -i 's/#Color/Color\nILoveCandy/g'                      /etc/pacman.conf
+
+# --- Enable parallel downloading
+sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /etc/pacman.conf
+
+# --- Enable multilib
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//'                  /etc/pacman.conf    # --- Enable multilib
